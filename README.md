7f95958ac2755b027238f549b7089cfec47a297f
plrenaudin <pl.renaudin@gmail.com>
2016-05-29 12:51:58 +0100

    original commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# goto-last-edit package
+
+This package is similar to [goto-last-edit](https://github.com/plrenaudin/goto-last-edit) package for Atom.
+
+This package keeps track of the edits location and allows you to move back and forward in the edits history
+
+Move the cursor to the **last** edit position when you type `ctrl+-`
+
+Move the cursor to the **next** edit position when you type `ctrl+=`
---
+++ extension.js
@@ -0,0 +1,67 @@
+var vscode = require('vscode');
+var history = [];
+var historyPosition = 0;
+var historyMaxSize = 50;
+var lastEditLocation;
+var editGroupTimeThreshold = 500;
+var lastEditTime;
+
+function _onEvent(e) {
+    var lastChangeTooRecent = Date.now() - lastEditTime < editGroupTimeThreshold;
+    var documentHasChanged = e.document.isDirty || e.document.isUntitled;
+    if (!lastChangeTooRecent && documentHasChanged) {
+        saveChangePosition(e);
+    }
+}
+
+function saveChangePosition(event) {
+    var changePosition = event.contentChanges[event.contentChanges.length - 1];
+    var change = {
+        position: changePosition.range,
+        file: event.document.uri.path
+    };
+
+    lastEditTime = Date.now();
+
+    if (historyPosition < history.length) {
+        history.splice(historyPosition, history.length - historyPosition);
+    }
+    if (history.length >= historyMaxSize) {
+        history.splice(0, history.length - historyMaxSize + 1);
+    }
+    history.push(change);
+    return historyPosition++;
+}
+
+function scrollToLocation(editor, edit) {
+    editor.revealRange(edit.position);
+    editor.selection = new vscode.Selection(edit.position.start, edit.position.end);
+}
+
+function navigate(goingBack) {
+    if (history.length > 0) {
+        lastEditLocation = goingBack ? history[historyPosition - 1] : history[historyPosition + 1];
+        if (lastEditLocation) {
+            historyPosition = goingBack ? historyPosition - 1 : historyPosition + 1;
+            var currentEditor = vscode.window.activeTextEditor;
+            if (currentEditor && currentEditor.document.fileName === lastEditLocation.file) {
+                scrollToLocation(currentEditor, lastEditLocation);
+            } else {
+                vscode.workspace.openTextDocument(lastEditLocation.file)
+                    .then(vscode.window.showTextDocument)
+                    .then(function (editor) { scrollToLocation(editor, lastEditLocation); });
+            }
+        }
+    }
+}
+
+module.exports = {
+    activate: function (context) {
+        var disposable = vscode.workspace.onDidChangeTextDocument(_onEvent);
+        var goBack = vscode.commands.registerCommand('extension.goBack', function () { navigate(true); });
+        var goForward = vscode.commands.registerCommand('extension.goForward', function () { navigate(false); });
+
+        context.subscriptions.push(disposable, goBack, goForward);
+    }
+};
+
